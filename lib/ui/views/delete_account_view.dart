b6365308d4adbf6d193f9f3eb76c8c5ffbd65c4e
abel3ri <abelmerete@yahoo.com>
2024-09-15 21:22:42 +0300

    refactor: import refactored widgets:

--- lib/ui/views/delete_account_view.dart
+++ lib/ui/views/delete_account_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:my_link_space/ui/shared/components/colors.dart';
 import 'package:my_link_space/ui/widgets/r_button.dart';
+import 'package:my_link_space/ui/widgets/r_text_form_field.dart';
 
 class DeleteAccountView extends StatelessWidget {
   const DeleteAccountView({super.key});
@@ -87,50 +87,6 @@ class DeleteAccountView extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class RTextField extends StatelessWidget {
-  const RTextField({
-    super.key,
-    required this.controller,
-    required this.hintText,
-    required this.keyboardType,
-    required this.textInputAction,
-    required this.validator,
-  });
-  final TextEditingController controller;
-  final String hintText;
-  final TextInputAction textInputAction;
-  final TextInputType keyboardType;
-  final String? Function(String? value)? validator;
-
-  @override
-  Widget build(BuildContext context) {
-    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
-    return TextFormField(
-      controller: controller,
-      decoration: InputDecoration(
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(6),
-          borderSide: BorderSide.none,
-        ),
-        filled: true,
-        fillColor:
-            isDarkMode ? textFieldFillColorDark : textFieldFillColorLight,
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(6),
-          borderSide: BorderSide.none,
-        ),
-        hintText: hintText,
-        hintStyle: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Colors.grey.shade500,
-            ),
-      ),
-      textInputAction: textInputAction,
-      keyboardType: keyboardType,
-      validator: validator,
     );
   }
 }
